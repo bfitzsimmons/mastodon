0a47fa7e8da7e7de565b1676748745ef441912c6
Brant Fitzsimmons <brant.fitzsimmons@gmail.com>
2017-11-06 11:29:31 -0500

    Added an initialization function to allow for connections, with context, to Postgres.

--- postgres.go
+++ postgres.go
@@ -8,16 +8,25 @@ import (
 )
 
 // InitializePostgres initializes the connections to the Postgres host.
-func InitializePostgres(ctx context.Context, config *Config) (*sqlx.DB, error) {
-	dataSourceName := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s connect_timeout=%d",
-		config.Host,
-		config.Port,
-		config.User,
-		config.Password,
-		config.Database,
-		config.SSLMode,
-		config.ConnectTimeout,
-	)
+func InitializePostgres(config *Config) (*sqlx.DB, error) {
+	dataSourceName := generateDataSourceName(config)
+
+	// Make the connection.
+	connection, err := sqlx.Connect("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the connection limits.
+	connection.SetMaxIdleConns(config.MaxIdleConnections)
+	connection.SetMaxOpenConns(config.MaxConnections)
+
+	return connection, nil
+}
+
+// InitializePostgresWithContext initializes the connections to the Postgres host -- with context.
+func InitializePostgresWithContext(ctx context.Context, config *Config) (*sqlx.DB, error) {
+	dataSourceName := generateDataSourceName(config)
 
 	// Make the connection.
 	connection, err := sqlx.ConnectContext(ctx, "postgres", dataSourceName)
@@ -30,4 +39,16 @@ func InitializePostgres(ctx context.Context, config *Config) (*sqlx.DB, error) {
 	connection.SetMaxOpenConns(config.MaxConnections)
 
 	return connection, nil
+}
+
+func generateDataSourceName(config *Config) string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s connect_timeout=%d",
+		config.Host,
+		config.Port,
+		config.User,
+		config.Password,
+		config.Database,
+		config.SSLMode,
+		config.ConnectTimeout,
+	)
 }
